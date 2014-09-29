71e0e950828a92d8cfe2a06ffa495d44166a1833
Rajesh <rajesh>
2014-09-29 17:26:05 +0530

    adding the fix for the issue #1

--- src/com/example/calculator/MainActivity.java
+++ src/com/example/calculator/MainActivity.java
@@ -1,24 +1,7 @@
 package com.example.calculator;
 
-import android.os.Bundle;
-import android.preference.EditTextPreference;
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import android.os.Bundle;
 /**
  * @author Rajesh.Vemula
  * Tasks pending
